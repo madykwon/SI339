450e1b0ba27de1c1e9084a6a0acd202228b6ba15
Madeline Kwon <madykwon@0587351504.wireless.umich.net>
2019-04-14 13:07:31 -0400

    changes

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ navBarToggle.addEventListener('click', function () {
 });
 
 
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 for (i = 0; i < acc.length; i++) {
@@ -19,6 +20,7 @@ for (i = 0; i < acc.length; i++) {
 	});
 }
 
+
 window.onscroll = function() {myFunction()};
 var navbar = document.getElementsByTagName("header")[0];
 var sticky = navbar.offsetTop;
@@ -29,3 +31,74 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+window.onload = function() {
+  typeWriter();
+};
+
+var i = 0;
+var txt = 'a travel diary';
+var speed = 70;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+  	// setInterval(function() {
+  	// 	if (i === 0) {
+  	// 		document.getElementById("type").innerHTML = txt + "|" 
+  	// 		i = 1;
+  	// 	} else {
+  	// 		document.getElementById("type").innerHTML = txt 
+  	// 		i = 0;
+  	// 	};
+  	// })
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
